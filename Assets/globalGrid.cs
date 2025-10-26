b5b16c224ca19481650156370485084e1adcbbc0
Charlie <charlie.rhodespeel@gmail.com>
2025-10-26 19:39:02 +0000

    Added a global grid

---
+++ Assets/globalGrid.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class globalGrid : MonoBehaviour
+{
+    [Header("Grid Perams")]
+    [SerializeField] private int length;
+    [SerializeField] private int width;
+    [SerializeField] private int cellSize;
+
+    public Vector3[,] grid;
+
+    // Make the global grid
+    private void Start()
+    {
+        grid = new Vector3[width, length];
+
+        Debug.Log("Start running");
+        //Assumes that Global Grid object is placed at the top left corner
+        for (int x = 0; x < width; x = x + cellSize)
+        {
+            for (int y = 0; y < length; y = y + cellSize)
+            {
+                grid[x, y] = new Vector3(transform.position.x + x, 0f,transform.position.z + y);
+            }
+        }
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        for (int x = 0; x < width; x = x + cellSize)
+        {
+            for (int y = 0; y < length; y = y + cellSize)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(grid[x, y], new Vector3(cellSize/2, cellSize/2, cellSize/2));
+            }
+        }
+    }
+}
