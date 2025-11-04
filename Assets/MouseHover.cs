65d83f9c8886e0c33be2ed541b3e22888ca4c8ee
Charlie <charlie.rhodespeel@gmail.com>
2025-11-04 16:11:16 +0000

    Character Faces Mouse and Mouse Position shown on grid

---
+++ Assets/MouseHover.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    [Header("Other Components")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject hoverIndicatorPrefab;
+
+    private GameObject hoverIndicator;
+
+    private Vector3 lastPosition;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        hoverIndicator = Instantiate(hoverIndicatorPrefab);
+    }
+    void Update()
+    {
+        Vector3 mousePos = GetSelectedMapPos();
+
+        //Using floor to do the grid visualisation
+        hoverIndicator.transform.position = new Vector3(Mathf.Floor(mousePos.x*0.5f)*2, 0.005f, Mathf.Floor(mousePos.z*0.5f)*2);
+    }
+    
+    public Vector3 GetSelectedMapPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Ray ray = camera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100, groundLayer))
+        {
+            lastPosition = hit.point;
+        }
+        return lastPosition;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("PlayerStats")]
     [SerializeField] private float speed = 10f; //[SerializeField] makes it show up in the editor but doesn't make it public to other scripts
 
+    [Header("Other Components")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask ground;
     private Rigidbody rb;
     private Vector3 input;
 
@@ -16,19 +20,37 @@ public class PlayerController : MonoBehaviour
 
     void Update() // Input called in the update
     {
+        // Movement
         input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+
+        PlayerRotation();
     }
     void FixedUpdate() //Actual movement in fixed update so isn't frame dependant
     {
-        
+        //Handles the movement 
+
         if (input != Vector3.zero)
         {
+            //The Vector3's are added so that the player moves on a 45 degree angle
+            Vector3 forward = new Vector3(1f, 0f, 1f).normalized * Input.GetAxis("Vertical");
+            Vector3 right = new Vector3(1f, 0f, -1f).normalized * Input.GetAxis("Horizontal");
 
-            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            Vector3 heading = Vector3.Normalize(dir * speed * Time.fixedDeltaTime);
+            transform.position += (forward + right).normalized * speed * Time.fixedDeltaTime;
+        }
+    }
+    
+    private void PlayerRotation()
+    {
+        Ray raycast = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-            transform.forward = heading;
-            transform.position += dir * speed * Time.fixedDeltaTime;
+        if(Physics.Raycast(raycast, out hit, Mathf.Infinity, ground))
+        {
+            Vector3 lookDir = hit.point - transform.position;
+            lookDir.y = 0;
+
+            transform.rotation = Quaternion.LookRotation(lookDir);
         }
     }
 
