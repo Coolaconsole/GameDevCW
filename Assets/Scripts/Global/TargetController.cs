bcfac378ac916f97e2603799bf938c04420d1dbb
Coolaconsole <93737386+Coolaconsole@users.noreply.github.com>
2025-11-12 13:05:33 +0000

    Fixed targetting

--- Assets/Scripts/Global/TargetController.cs
+++ Assets/Scripts/Global/TargetController.cs
@@ -16,19 +16,19 @@ public class TargetController : MonoBehaviour
             possibleTargets.Add(other.gameObject);
     }
 
-    private void Update()
-    {
-        for (int i = possibleTargets.Count - 1; i >= 0; i--)
-        {
-            GameObject target = possibleTargets[i];
-            if (Vector3.Distance(transform.position, target.transform.position) > GetComponent<SphereCollider>().radius)
-            {
-                possibleTargets.RemoveAt(i);
-                if (currentTarget == target)
-                    currentTarget = null;
-            }
-        }
-    }
+    // private void Update()
+    // {
+    //     for (int i = possibleTargets.Count - 1; i >= 0; i--)
+    //     {
+    //         GameObject target = possibleTargets[i];
+    //         if (Vector3.Distance(transform.position, target.transform.position) > GetComponent<SphereCollider>().radius)
+    //         {
+    //             possibleTargets.RemoveAt(i);
+    //             if (currentTarget == target)
+    //                 currentTarget = null;
+    //         }
+    //     }
+    // }
 
     private void OnTriggerExit(Collider other)
     {
